cc5b3c93b87a1837d4e05f963f185ce95a007c92
JayvyLei Difuntorum <jdifuntorum@jdifuntorum-macbookpro2.roam.corp.google.com>
2018-09-05 06:30:21 -0700

    init commit

---
+++ javaJokesLib/src/main/java/com/nanodegree/android/javajokeslib/JavaJokes.java
@@ -0,0 +1,8 @@
+package com.nanodegree.android.javajokeslib;
+
+public class JavaJokes {
+    public String getJoke() {
+        String magic_trick_joke = "Who wants to see a magic trick!?!";
+        return magic_trick_joke;
+    }
+}
